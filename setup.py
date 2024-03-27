f68565be7b9c5d73287c994958c9f53b42491404
gerish01 <gerish.r@tigeranalytics.com>
2024-03-27 10:48:41 +0530

    trail

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="ERROR_1.3_TO_CHECK-1",
+    name="HousePricePrediction",
     version="0.0.1",
     install_requires=[
         "requests",
---
+++ test/functional_test/test_package.py
@@ -0,0 +1,8 @@
+def test_pkg_import():
+    try:
+        import awesome_package  # noqa
+    except Exception as e:
+        assert False, (
+            f"Error: {e}. "
+            "mypackage package is not imported and installed correctly."
+        )
